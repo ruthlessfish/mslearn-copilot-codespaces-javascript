e152a913303123db6a9b266c5277ee936cb9c11c
Shane Pearson <bubbafoley@gmail.com>
2024-05-21 19:42:24 +0000

    add my changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shane Pearson",
+  title: "Codeslinger",
+  email: "bubbafoley@gmail.com",
+  gitHub: "ruthlessfish",
+  instagram: "shane-pearson-***123",
+  linkedIn: "shane-pearson-***123",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "bubbafoley",
+  youTube: "bubbafoley",
 };
 
 const primaryColor = "#4E567E";
